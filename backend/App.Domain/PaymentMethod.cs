9befa4f3e2f760023212c661f91162c13bba7e99
stlaan <stlaan@ttu.ee>
2024-04-10 21:27:43 +0300

    Fixed PaymentMethod not inheriting baseclass

--- backend/App.Domain/PaymentMethod.cs
+++ backend/App.Domain/PaymentMethod.cs
@@ -1,6 +1,8 @@
+using Base.Domain;
+
 namespace App.Domain;
 
-public class PaymentMethod
+public class PaymentMethod : BaseEntityIdMetadata
 {
     public string MethodName { get; set; } = default!;
     public string MethodDescription { get; set; } = default!;
