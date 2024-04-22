fee4882c3cc34e46860b6486a65f25433bca434f
StenMartin <stenmartinlaane@gmail.com>
2024-04-22 03:18:39 +0300

    changes for visual studio.

--- backend/App.DAL.DTO/ParticipantEvent.cs
+++ backend/App.DAL.DTO/ParticipantEvent.cs
@@ -1,22 +1,23 @@
 using System.Text.Json.Serialization;
 using Base.Domain;
 
-namespace App.DAL.DTO;
+namespace App.DAL.DTO {
+    public class ParticipantEvent : BaseEntityId
+    {
+        public DateTime RegisterDateTime { get; set; }
 
-public class ParticipantEvent : BaseEntityId
-{
-    public DateTime RegisterDateTime { get; set; }
-    
-    public Guid? PersonId { get; set; }
-    public Person? Person { get; set; }
-    
-    public Guid? FirmId { get; set; }
-    public Firm? Firm { get; set; }
-    
-    public Guid? EventId { get; set; }
-    [JsonIgnore]
-    public Event? Event { get; set; }
-    
-    public Guid? PaymentMethodId { get; set; }
-    public PaymentMethod? PaymentMethod { get; set; }
+        public Guid? PersonId { get; set; }
+        public Person? Person { get; set; }
+
+        public Guid? FirmId { get; set; }
+        public Firm? Firm { get; set; }
+
+        public Guid? EventId { get; set; }
+        [JsonIgnore]
+        public Event? Event { get; set; }
+
+        public Guid? PaymentMethodId { get; set; }
+        public PaymentMethod? PaymentMethod { get; set; }
+    }
 }
+
--- backend/App.Domain/Firm.cs
+++ backend/App.Domain/Firm.cs
@@ -2,20 +2,22 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Base.Domain;
 
-namespace App.Domain;
-
-public class Firm : BaseEntityIdMetadata
+namespace App.Domain
 {
-    [MaxLength(256)]
-    public string Name { get; set; } = default!;
-    
-    [MaxLength(256)]
-    public string RegistryCode { get; set; } = default!;
-    public int ParticipantCount { get; set; }
-    
-    [MaxLength(5000)]
-    public string AdditionalNotes { get; set; } = default!;
-    
-    [JsonIgnore]
-    public ICollection<ParticipantEvent>? ParticipantEvents { get; set; }
+
+    public class Firm : BaseEntityIdMetadata
+    {
+        [MaxLength(256)]
+        public string Name { get; set; } = default!;
+
+        [MaxLength(256)]
+        public string RegistryCode { get; set; } = default!;
+        public int ParticipantCount { get; set; }
+
+        [MaxLength(5000)]
+        public string AdditionalNotes { get; set; } = default!;
+
+        [JsonIgnore]
+        public ICollection<ParticipantEvent>? ParticipantEvents { get; set; }
+    }
 }
